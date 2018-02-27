a6f06648f2d983f5430cb58dd9a2b1ecd12c0899
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2018-02-26 23:43:48 -0500

    Add files via upload

---
+++ docs/02_Courseware/powershell/PowerShell MSI.ps1
@@ -0,0 +1,15 @@
+﻿# Get an access token for the MSI
+$response = Invoke-WebRequest -Uri http://localhost:50342/oauth2/token `
+                              -UseBasicParsing `
+                              -Method GET -Body @{resource="https://management.azure.com/"} -Headers @{Metadata="true"}
+$content =$response.Content | ConvertFrom-Json
+$access_token = $content.access_token
+echo "The MSI access token is $access_token"
+
+# Use the access token to get resource information for the VM
+$vmInfoRest = (Invoke-WebRequest -Uri https://management.azure.com/subscriptions/<SUBSCRIPTION-ID>/resourceGroups/<RESOURCE-GROUP>/providers/Microsoft.Compute/virtualMachines/<VM-NAME>?api-version=2017-12-01 -Method GET -ContentType "application/json" -Headers @{ Authorization ="Bearer $access_token"}).content
+echo "JSON returned from call to get VM info:"
+echo $vmInfoRest
+
+# Use the access token to sign in under the MSI service principal. -AccountID can be any string to identify the session.
+Login-AzureRmAccount -AccessToken $access_token -AccountId "MSI@50342"
---
+++ docs/02_Courseware/powershell/SimpleVM.ps1
@@ -0,0 +1,17 @@
+﻿$time = get-date
+
+Write-Host $time
+
+New-AzureRmVm `
+    -ResourceGroupName "myResourceGroup1" `
+    -Name "myVM$(Get-Random)" `
+    -Location "East US" `
+    -VirtualNetworkName "myVnet1" `
+    -SubnetName "mySubnet1" `
+    -SecurityGroupName "myNetworkSecurityGroup1" `
+    -PublicIpAddressName "myPublicIpAddress1" `
+    -OpenPorts 80,3389
+
+$time = get-date
+
+Write-Host $time
